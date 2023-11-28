ac508295230d97bdf3f5201ce5ba0b4e67ac094d
wzl <1367112248@qq.com>
2023-11-28 22:31:48 +0800

    bugfix

--- src/main/java/com/manage/qq/enums/CommandEnum.java
+++ src/main/java/com/manage/qq/enums/CommandEnum.java
@@ -13,7 +13,7 @@ public enum CommandEnum {
     COMMAND_N2N_RESTART(null, null, "重启n2n", "【重启n2n】：关闭并重新启动n2n"),
     COMMAND_N2N_STOP(null, null, "关闭n2n", "【关闭n2n】：关闭n2n的运行"),
     COMMAND_SHOOT_SCREEN(null, null, "截屏", "【截屏】：直接在服务器上截图"),
-    COMMAND_GPT(" ", "  ", null, "【  +${聊天}】和GPT聊天"),
+    COMMAND_GPT(" ", "  ", null, "【 +${聊天}】和GPT聊天"),
     COMMAND_LEISHEN_INFO(null, null, "查看雷神", "【查看雷神】查看雷神状态"),
     COMMAND_LEISHEN_PAUSE(null, null, "暂停雷神", "【暂停雷神】暂停雷神时长"),
     ;
